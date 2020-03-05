4e5324a994b16fd6f8a2e042bfef79bfe3818513
zen_n <zenhuangzheng@126.com>
2020-03-05 19:35:28 +0800

    Update vue2do document website.

--- static/js/37.bundle.4ae940f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[37],{506:function(n,e,t){t(124),n.exports=function(n){return""+'<div class="page-about">hello, 我可以撩你吗？</div>'}},570:function(n,e,t){"use strict";t.r(e);var a=t(506),o=t.n(a);e.default={name:"PageHello",template:o()(),data:function(){return{}}}}}]);
